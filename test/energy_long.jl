ce1d49b6726858efc830f64728692154f610a913
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2024-02-06 16:56:48 +0800

    revise test energy_long

--- test/energy_long.jl
+++ test/energy_long.jl
@@ -16,9 +16,9 @@
     k_x, k_y, r_z, us_mat, H_r, H_c, H_s, ivsm, b_l, b_u, rhs, sol, sort_z, z = FFCT_precompute(L, N_grid, USeriesPara(2), M_mid, n_atoms)
 
     @info "running the FFCT for the long range part of the energy"
-    E_FFCT = energy_long(qs, poses, L, 3, k_x, k_y, r_z, z, sort_z, us_mat, b_l, b_u, rhs, sol, ivsm, H_r, H_c, H_s, uspara, soepara)
+    E_FFCT = energy_long(qs, poses, L, M_mid, k_x, k_y, r_z, z, sort_z, us_mat, b_l, b_u, rhs, sol, ivsm, H_r, H_c, H_s, uspara, soepara)
 
-    @info "running the direct summation for the middle range part of the energy"
+    @info "running the direct summation for the long range part of the energy"
     # using the direct summation
     E_direct = long_energy_us(qs, poses, 30, L, uspara, M_mid + 1, length(uspara.sw))
 
