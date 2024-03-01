1f8a742a580c17e6b55e6a2a75e32f3db0f2d491
Xuanzhao Gao <gaoxuanzhao@gmail.com>
2024-03-01 10:56:41 +0800

    added taylor, but not working weel

--- src/FastSpecSoG.jl
+++ src/FastSpecSoG.jl
@@ -9,13 +9,13 @@ export mid_paras_gen, energy_mid
 export FFCT_precompute, boundaries!, inverse_mat
 export interpolate_nu_loop_single!, interpolate_nu_loop!, interpolate_nu_einsum!, interpolate_nu_einsum_non_inplace!, interpolate_nu_taylor!, interpolate_nu_taylor_single!
 export real2Cheb!, solve_eqs!
-export ather_nu, gather_nu_single
+export gather_nu, gather_nu_single
 export zeroth_order
 export energy_long_loop_k, energy_long_einsum_k
 export long_energy_us_k, long_energy_us_0, long_energy_sw_0, long_energy_sw_k
 export energy_long_0
 export SoePara, SoePara4, SoePara8, SoePara16
-export TaylorSeriesPara, Taylor_USeries, TaylorUSeries
+export TaylorSeriesPara, TaylorUSeries, TaylorUSeries_k, USeries_direct, horner
 
 include("types.jl")
 
